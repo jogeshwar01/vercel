6e5115dd7c68e00901427c7eabe2317dbea4a2be
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-03-14 20:41:46 +0530

    feat: redis and socket.io for logs

--- apps/api-build/script.js
+++ apps/api-build/script.js
@@ -3,6 +3,18 @@ import path from "path";
 import fs from "fs";
 import mime from "mime-types";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import Redis from 'ioredis';
+const publisher = new Redis()
+const PROJECT_ID = process.env.PROJECT_ID
+
+async function publishLog(log) {
+    try {
+        const result = await publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+        console.log(`Publish result: ${result}`);
+    } catch (error) {
+        console.error(`Error publishing log: ${error}`);
+    }
+}
 
 const s3Client = new S3Client({
     region: process.env.AWS_REGION_NAME ?? "aws-region-missing",
@@ -23,23 +35,30 @@ async function init() {
 
     command.stdout?.on("data", function (data) {
         console.log(data.toString());
+        publishLog(data.toString())
     });
 
     command.stdout?.on("error", function (data) {
         console.log(data.toString());
+        publishLog(data.toString())
     });
 
     command.on("close", async function () {
         console.log("Build Complete");
+        publishLog(`Build Complete`)
 
         const distFolderPath = path.join(__dirname, "output", "dist");
         const distFolderContents = fs.readdirSync(distFolderPath, {
             recursive: true,
         });
 
+        publishLog(`Starting to upload`)
         for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file.toString()); // toString() for buffers
             if (fs.lstatSync(filePath).isDirectory()) continue;
+
+            console.log('uploading', filePath)
+            publishLog(`uploading ${file}`)
 
             const command = new PutObjectCommand({
                 Bucket: process.env.AWS_S3_BUCKET,
@@ -49,9 +68,11 @@ async function init() {
             });
 
             await s3Client.send(command);
+            publishLog(`uploaded ${file}`)
             console.log("uploaded", filePath);
         }
 
+        publishLog(`Done`)
         console.log("Done...");
     });
 }
--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,12 +1,29 @@
 import express from 'express';
 import { generateSlug } from 'random-word-slugs';
 import { ECSClient, RunTaskCommand } from '@aws-sdk/client-ecs';
+import { Server } from 'socket.io';
+import Redis from 'ioredis';
 import dotenv from "dotenv";
 import cors from "cors"
 dotenv.config();
 
 const app = express()
 const PORT = 9001
+
+
+const subscriber = new Redis()
+
+const io = new Server({ cors: { origin: "*" }});
+
+io.on('connection', socket => {
+    socket.on('subscribe', channel => {
+        socket.join(channel)
+        socket.emit('message', `Joined ${channel}`)
+    })
+})
+
+io.listen(9002, {})
+console.log('Socket Server running on port 9002');
 
 const ecsClient = new ECSClient({
     region: process.env.AWS_REGION_NAME ?? "aws-region-missing",
@@ -63,5 +80,16 @@ app.post('/project', async (req, res) => {
     return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:${process.env.PROXY_PORT}` } }) // 9000 is s3 reverse proxy port
 
 })
+
+async function initRedisSubscribe() {
+  console.log('Subscribed to logs....')
+
+  subscriber.psubscribe('logs:*') // all channels starting with logs:
+  subscriber.on('pmessage', (pattern, channel, message) => {
+      io.to(channel).emit('message', message)
+  })
+}
+
+initRedisSubscribe()
 
 app.listen(PORT, () => console.log(`API Server Running..${PORT}`))
